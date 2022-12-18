35995347ad2fa56f110883a0f91a386b8491d769
ashishverma4822 <35832574+ashishverma4822@users.noreply.github.com>
2022-12-18 12:00:19 +0530

    Create Daily_Temperatures.cpp

---
+++ Daily_Temperatures.cpp
@@ -0,0 +1,30 @@
+//Ashish Kumar Verma
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> v;
+        stack<pair<int,int>>s;
+        for(int i=temperatures.size()-1;i>=0;i--){
+            if(s.size()==0){
+                v.push_back(0);
+            }
+            else if(s.size()>0 and s.top().first>temperatures[i]){
+                v.push_back(s.top().second-i);
+            }
+            else if(s.size()>0 and s.top().first<=temperatures[i]){
+                while(s.size()>0 and s.top().first<=temperatures[i]){
+                    s.pop();
+                }
+                if(s.size()==0){
+                    v.push_back(0);
+                }
+                else{
+                    v.push_back(s.top().second-i);
+                }
+            }
+            s.push({temperatures[i],i});
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
